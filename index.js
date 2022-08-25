d2a25de24ec714df47e0fe19798b7947ca1f1435
DawkinsJonathanOchieng <dawkinsjonathan871@gmail.com>
2022-08-25 09:28:19 +0300

    commit tree 1f7b4deb5f2b53fe38e14cb6c19e3e70aefc7b68

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blockNumber) {
+    if (blockNumber >= 43)
+    return blockNumber - 42
+
+    else if (blockNumber <= 41)
+    return 42 - blockNumber
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return ((Math.abs(start - destination)) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    }
+    if (
+        distanceTravelledInFeet(start, destination) > 400 &&
+        distanceTravelledInFeet(start, destination) < 2000
+    ) {
+        return 2.56;
+    }
+    if (
+        distanceTravelledInFeet(start, destination) > 2000 && 
+        distanceTravelledInFeet(start, destination) < 2500
+    ) {
+        return 25;
+    }
+    if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far";
+    }
+}
